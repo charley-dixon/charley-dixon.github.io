cfb4459e576fe03476664b918096c000cc995a3f
Charley Dixon <dixoncharley@gmail.com>
2022-03-02 20:25:49 -0700

    Fix hamburger menu toggle to ensure it works upon page load.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const hamburger = document.getElementById('menu-toggle');
 const nav = document.querySelector('nav');
 
+// the visibility value was '' upon page loading so I am setting it here before the toggleNav function runs for the first time just to make sure it starts out as "hidden" every time the page loads
+nav.style.visibility = 'hidden';
+
 function toggleNav() {
+    console.log('HERE!');
     if (nav.style.visibility === "hidden") {
         nav.style.visibility = "visible";
         nav.style.opacity = "1";
@@ -15,6 +19,8 @@ hamburger.addEventListener('click', function() {
     toggleNav();
 });
 
+
+// Code to make sure nav closes after a link is clicked
 const navLinks = document.querySelectorAll('.nav-link');
 
 for(let i = 0 ; i < navLinks.length ; i++) {
